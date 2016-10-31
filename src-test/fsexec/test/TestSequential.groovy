204e4abc648a9977077d2e8b4945d8d67d695a71
carlemueller <carlemueller@yahoo.com>
2016-10-31 02:36:33 -0500

    seq mode - WORKING

---
+++ src-test/fsexec/test/TestSequential.groovy
@@ -0,0 +1,13 @@
+package fsexec.test
+
+import fsexec.compile.FSEFlow
+import fsexec.compile.FSExecCompiler
+import fsexec.execute.FSExecutor
+
+
+FSExecCompiler compiler = new FSExecCompiler()
+FSExecutor executor = new FSExecutor()
+
+FSEFlow flow = compiler.compile("./test-flows/code/test-seq",null,"./test-flows/work/test-seq",null,null)
+executor.exec(flow)
+
